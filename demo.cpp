163aa02a3db9c03d0b247fb249ab2b5e6e872f03
Jared Hoberock <jaredhoberock@gmail.com>
2020-03-05 17:21:53 -0600

    Put get in TUPLE_NAMESPACE instead of std

--- demo.cpp
+++ demo.cpp
@@ -29,10 +29,10 @@ int main()
   ));
 
   type1 t1;
-  assert(std::get<0>(t1) == 0);
+  assert(get<0>(t1) == 0);
 
   type1 t2(13);
-  assert(std::get<0>(t2) == 13);
+  assert(get<0>(t2) == 13);
 
   assert(t1 <= t2);
   assert(t1 <  t2);
@@ -60,17 +60,17 @@ int main()
   ));
 
   type2 t3;
-  assert(std::get<0>(t3) == 0);
-  assert(std::get<1>(t3) == 0);
+  assert(get<0>(t3) == 0);
+  assert(get<1>(t3) == 0);
 
   type2 t4(13,7);
-  assert(std::get<0>(t4) == 13);
-  assert(std::get<1>(t4) == 7);
+  assert(get<0>(t4) == 13);
+  assert(get<1>(t4) == 7);
 
 
   assert((
     std::is_same<
-      decltype(std::get<0>(t3)),
+      decltype(get<0>(t3)),
       int&
     >::value
   ));
@@ -78,7 +78,7 @@ int main()
 
   assert((
     std::is_same<
-      decltype(std::get<1>(t3)),
+      decltype(get<1>(t3)),
       float&
     >::value
   ));
@@ -86,7 +86,7 @@ int main()
 
   assert((
     std::is_same<
-      decltype(std::get<0>(static_cast<const type2&>(t3))),
+      decltype(get<0>(static_cast<const type2&>(t3))),
       const int&
     >::value
   ));
@@ -94,7 +94,7 @@ int main()
 
   assert((
     std::is_same<
-      decltype(std::get<1>(static_cast<const type2&>(t3))),
+      decltype(get<1>(static_cast<const type2&>(t3))),
       const float&
     >::value
   ));
@@ -102,7 +102,7 @@ int main()
 
   assert((
     std::is_same<
-      decltype(std::get<0>(std::move(t3))),
+      decltype(get<0>(std::move(t3))),
       int&&
     >::value
   ));
@@ -110,21 +110,21 @@ int main()
 
   assert((
     std::is_same<
-      decltype(std::get<1>(std::move(t3))),
+      decltype(get<1>(std::move(t3))),
       float&&
     >::value
   ));
       
   using type3 = tuple<int,int,int>;
   type3 t5(1,2,3);
-  assert(std::get<0>(t5) == 1);
-  assert(std::get<1>(t5) == 2);
-  assert(std::get<2>(t5) == 3);
+  assert(get<0>(t5) == 1);
+  assert(get<1>(t5) == 2);
+  assert(get<2>(t5) == 3);
 
   type3 t6{};
-  assert(std::get<0>(t6) == 0);
-  assert(std::get<1>(t6) == 0);
-  assert(std::get<2>(t6) == 0);
+  assert(get<0>(t6) == 0);
+  assert(get<1>(t6) == 0);
+  assert(get<2>(t6) == 0);
 
   assert(t5 != t6);
   assert(t5 >  t6);
@@ -133,9 +133,9 @@ int main()
   assert(t6 <= t5);
 
   t6 = t5;
-  assert(std::get<0>(t6) == 1);
-  assert(std::get<1>(t6) == 2);
-  assert(std::get<2>(t6) == 3);
+  assert(get<0>(t6) == 1);
+  assert(get<1>(t6) == 2);
+  assert(get<2>(t6) == 3);
 
   assert(t5 == t6);
   assert(t5 <= t6);
@@ -143,18 +143,18 @@ int main()
 
   using type4 = tuple<type3,type3>;
   type4 t7(type3(1,2,3), type3(4,5,6));
-  assert(std::get<0>(std::get<0>(t7)) == 1);
-  assert(std::get<1>(std::get<0>(t7)) == 2);
-  assert(std::get<2>(std::get<0>(t7)) == 3);
+  assert(get<0>(get<0>(t7)) == 1);
+  assert(get<1>(get<0>(t7)) == 2);
+  assert(get<2>(get<0>(t7)) == 3);
 
-  assert(std::get<0>(std::get<1>(t7)) == 4);
-  assert(std::get<1>(std::get<1>(t7)) == 5);
-  assert(std::get<2>(std::get<1>(t7)) == 6);
+  assert(get<0>(get<1>(t7)) == 4);
+  assert(get<1>(get<1>(t7)) == 5);
+  assert(get<2>(get<1>(t7)) == 6);
 
   type3 t8(1,2,3);
-  assert(std::get<0>(t5) == 1);
-  assert(std::get<1>(t5) == 2);
-  assert(std::get<2>(t5) == 3);
+  assert(get<0>(t5) == 1);
+  assert(get<1>(t5) == 2);
+  assert(get<2>(t5) == 3);
 
   int a, b, c;
   tie(a,b,c) = t5;
@@ -169,8 +169,8 @@ int main()
   auto t9 = make_tuple(std::string("hi"));
   tuple<std::string> t10;
   t10 = std::move(t9);
-  assert(std::get<0>(t9) == "");
-  assert(std::get<0>(t10) == "hi");
+  assert(get<0>(t9) == "");
+  assert(get<0>(t10) == "hi");
 
   assert(make_tuple(1,2,3) < make_tuple(1,2,4));
   assert(make_tuple(1,2,4) > make_tuple(1,2,3));
@@ -193,14 +193,14 @@ int main()
 
   {
     auto t = make_tuple(ignore, make_tuple(ignore));
-    std::get<0>(t);
+    get<0>(t);
   }
 
   {
     auto t = make_tuple(1, 13., std::string("hello"));
-    assert(std::get<int>(t) == 1);
-    assert(std::get<double>(t) == 13.);
-    assert(std::get<std::string>(t) == "hello");
+    assert(get<int>(t) == 1);
+    assert(get<double>(t) == 13.);
+    assert(get<std::string>(t) == "hello");
   }
 
   std::cout << "OK" << std::endl;
